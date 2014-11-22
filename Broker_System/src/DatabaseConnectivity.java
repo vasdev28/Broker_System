452968ffd0156a0a8c727a29e2fada21831f3e64
Anukul Kumar <anukulkumar91@gmail.com>
2014-11-22 17:45:26 -0600

    Added Dbconnectivity.java

---
+++ Broker_System/src/DatabaseConnectivity.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DatabaseConnectivity {
+	
+	public  Connection connectToDatabase() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+	String url = "jdbc:mysql://localhost:3306/"; 
+	String dbName = "netsec";
+	String driver = "com.mysql.jdbc.Driver"; 
+	String userName = "root";
+	String password = "root";
+		Class.forName(driver).newInstance(); 
+		Connection connection = DriverManager.getConnection(url+dbName,userName,password); 
+		
+		return connection;
+	}
+}
